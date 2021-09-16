19731c67c082a9af6f44fd6442ad796edf4cac0c
RushikeshGade <rushyagade7741@gmail.com>
2021-09-16 12:30:34 +0530

    Done

--- cypress/integration/examples/BDD/SetpDefinitions/Orangehrm.js
+++ cypress/integration/examples/BDD/SetpDefinitions/Orangehrm.js
@@ -1,5 +1,4 @@
 /// <reference types="Cypress" />
-import Orangehrm from '../SetpDefinitions/Orangehrm'
 import LoginPage from '../PageObject/LoginPage'
 import Hrms1page from '../PageObject/EmpolyeeSearch'
 import ViewEmployeeList from '../PageObject/ViewEmployeeList';
